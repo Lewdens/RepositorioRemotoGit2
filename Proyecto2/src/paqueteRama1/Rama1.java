ebaffa8d07327af3d0ecc6eecd4e9234b68ca66d
horabaixa <horabaixa@daw1-32>
2024-03-05 17:31:24 +0100

    v1.1

---
+++ Proyecto2/src/paqueteRama1/Rama1.java
@@ -0,0 +1,10 @@
+package paqueteRama1;
+
+public class Rama1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Versión 1.1");
+		System.out.println("Localizado en la rama1");
+	}
+}
